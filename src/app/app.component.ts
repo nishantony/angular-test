b1df02cc6d4b4c8500de40b07165f24a2c17991d
nishantony <nishaantony97@gmail.com>
2020-08-23 15:46:06 +0200

    zxczxc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularTest';
+  userLoggedIn = true;
+
+  greet() {
+    alert('Hey there!');
+  }
 }
--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
 
+  @Input() loggedIn: boolean;
+  // logginIn = true;
   constructor() { }
 
   ngOnInit() {
